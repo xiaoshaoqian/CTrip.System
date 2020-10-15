67f20b7d3a0fbe1895e1f33122fdc30f945f5482
xiaoshaoqian@fiberhome.com <99253857>
2020-10-15 17:41:45 +0800

    初步创建

---
+++ CTrip.System.Model/Api/ApiResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTrip.System.Model.Api
+{
+    /// <summary>
+    /// 统一接口返回
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiResult
+    {
+
+        public ApiResult()
+        {
+            StatusCode = 0;
+        }
+
+        /// <summary>
+        /// 请求状态
+        /// </summary>
+        public int StatusCode { get; set; }
+
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 返回时间戳
+        /// </summary>
+        public string Timestamp { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+
+    }
+
+    public class ApiResult<T> : ApiResult
+    {
+        /// <summary>
+        /// 接口返回值
+        /// </summary>
+        public T Data;
+
+    }
+}
---
+++ CTrip.System.Model/Api/PageParm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CTrip.System.Model.Api
+{
+    public class PageParm
+    {
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int PageIndex { get; set; } = 1;
+
+        /// <summary>
+        /// 每页总条数
+        /// </summary>
+        public int PageSize { get; set; } = 20;
+
+        /// <summary>
+        /// 排序字段
+        /// </summary>
+        public string OrderBy { get; set; }
+
+        /// <summary>
+        /// 排序方式
+        /// </summary>
+        public string Sort { get; set; }
+
+    }
+}
