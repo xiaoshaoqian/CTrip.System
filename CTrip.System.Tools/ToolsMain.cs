11825b11c312a6e3a3217dd880f566b72381d38e
xiaoshaoqian@fiberhome.com <99253857>
2020-10-16 16:17:49 +0800

    完成model,服务，服务接口，Dbcontext等类的自动生成

--- CTrip.System.Tools/ToolsMain.cs
+++ CTrip.System.Tools/ToolsMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CTrip.System.Interfaces.Service;
+using System;
 
 namespace CTrip.System.Tools
 {
@@ -6,7 +7,19 @@ namespace CTrip.System.Tools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tables = new ToolsService().GetAllTables();
+            foreach(var table in tables)
+            {
+                Console.Write($"生成[{ table }]表 模型: ");
+                Console.WriteLine(new ToolsService().CreateModels("..\\..\\..\\..\\CTrip.System.Model\\Entity", "CTrip.System.Model", table, ""));
+                Console.Write($"生成[{ table }]表 服务: ");
+                Console.WriteLine(new ToolsService().CreateServices("..\\..\\..\\..\\CTrip.System.Interfaces\\Service", "CTrip.System.Interfaces", table));
+                Console.Write($"生成[{ table }]表 接口: ");
+                Console.WriteLine(new ToolsService().CreateIServices("..\\..\\..\\..\\CTrip.System.Interfaces\\IService", "CTrip.System.Interfaces", table));
+            }
+            Console.Write($"生成DbContext: ");
+            Console.WriteLine(new ToolsService().CreateDbContext("..\\..\\..\\..\\CTrip.System.Core\\DbContext.cs", "CTrip.System.Core"));
+            Console.ReadKey();
         }
     }
 }
