b20f3eac4c92c22443687ff85d752106861b280e
Mr. Xiao <qiange12@qq.com>
2020-11-02 22:46:28 +0800

    完成登录过滤器

--- CTrip.System.Hostd/Authorization/AuthorizationAttribute.cs
+++ CTrip.System.Hostd/Authorization/AuthorizationAttribute.cs
@@ -1,5 +1,10 @@
-﻿using CTrip.System.Hostd.Extensions;
+﻿using CTrip.System.Common.Helpers;
+using CTrip.System.Hostd.Extensions;
+using CTrip.System.Model;
+using CTrip.System.Model.Api;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +19,52 @@ namespace CTrip.System.Hostd.Authorization
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            //var _tokenManager = context.HttpContext.RequestServices.GetService<TokenManager>();
+            #region 判断是否登录
+            var _tokenManager = context.HttpContext.RequestServices.GetService<TokenManager>();
+            if (!_tokenManager.IsAuthenticated())
+            {
+                ApiResult response = new ApiResult
+                {
+                    StatusCode = (int)StatusCodeType.Unauthorized,
+                    Message = StatusCodeType.Unauthorized.GetEnumText()
+                };
+                context.Result = new JsonResult(response)
+                {
+                    StatusCode = (int)StatusCodeType.Success
+                };
+                return;
+            }
+            #endregion
+
+            #region 判断是否拥有权限
+            if (!string.IsNullOrEmpty(Power))
+            {
+                if (Convert.ToBoolean(AppSettings.Configuration["AppSettings:Demo"]))
+                {
+                    ApiResult response = new ApiResult
+                    {
+                        StatusCode = (int)StatusCodeType.Error,
+                        Message = "当前为演示模式 , 您无权修改任何数据"
+                    };
+                    context.Result = new JsonResult(response) { StatusCode = (int)StatusCodeType.Success };
+                    return;
+                }
+                if (!_tokenManager.GetSessionInfo().UserPower.Contains(Power))
+                {
+                    ApiResult response = new ApiResult
+                    {
+                        StatusCode = (int)StatusCodeType.Forbidden,
+                        Message = StatusCodeType.Forbidden.GetEnumText()
+                    };
+
+                    context.Result = new JsonResult(response) { StatusCode = (int)StatusCodeType.Success };
+                    return;
+                }
+            }
+            #endregion
+
         }
 
+        
     }
 }
